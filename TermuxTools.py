9d1cc134b5ca2f6772dc94f0270245037042947e
Aizek05 <48653898+Aizek05@users.noreply.github.com>
2019-03-17 22:25:55 +0530

    Add files via upload

---
+++ TermuxTools.py
@@ -0,0 +1,16 @@
+file1 = "TermuxTools(eng).txt"
+startfile1 = open(file1, mode = 'r', encoding = 'ascii')
+print (startfile1.read())
+
+#-------------------------------------------------------------------------------
+c = int(input("choose the command:"))
+if c==1:
+	print('paste its: apt upgrade')
+if c==2:
+	print('paste its: termux-setup-storage')
+if c==3:
+	print('paste its: apt install python2')
+if c==4:
+	print('paste its: cd /storage/emulated/0')
+if c>4:
+	print('invalid number!')
